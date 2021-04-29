93bf14627c7159b7007ee8a06bcf70aef650a6a1
PeterG18 <81383703+PeterG18@users.noreply.github.com>
2021-04-29 19:35:58 -0400

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -3,6 +3,7 @@ charindex = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
 shift = int(sys.argv[1])
 result = ""
 string = sys.argv[2]
+string.lower()
 for char in string:
   num = charindex.index(char)
   if char.isalpha() == True:

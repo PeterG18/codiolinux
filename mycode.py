a14ea5dc3ca3ad3aa752f0f745ca659b301c1127
PeterG18 <81383703+PeterG18@users.noreply.github.com>
2021-04-29 19:35:02 -0400

    Update mycode.py

--- mycode.py
+++ mycode.py
@@ -1,14 +1,14 @@
+import sys
 charindex = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
-def encode(string, shift):
-  string.lower()
-  result = ""
-  for char in string:
-    num = charindex.index(char)
-    if char == " ":
-      result+= char
+shift = int(sys.argv[1])
+result = ""
+string = sys.argv[2]
+for char in string:
+  num = charindex.index(char)
+  if char.isalpha() == True:
     elif (num + shift) > 25:
       result+= char
     else:
       char = charindex[num+shift]
       result += char
-    return print(result)
+print(result)

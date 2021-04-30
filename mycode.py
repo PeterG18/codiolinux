97a3626c5df606b36927736ad948e285d7a1f488
peter gansallo <peter.gansallo@gmail.com>
2021-04-30 00:50:01 +0000

    final tweaks

--- mycode.py
+++ mycode.py
@@ -1,18 +1,18 @@
 import sys
-charindex = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+charindex = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 shift = int(sys.argv[1])
 result = ""
 string = sys.stdin.readline()
 for char in string:
-  num = charindex.index(char)
   if char.isalpha() == True:
+    num = charindex.index(char)
     if (num + shift) > 25:
       char = charindex[num+shift-25]
       result+= char
     else:
       char = charindex[num+shift]
       result += char
-   else:
-      result+= char
+  else:
+    result+= char
 x = result.upper()
 print(x)
